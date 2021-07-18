69048422d3e5ccfc3b9775bf14369a828dfa4d84
Jesse Antonio Pina Carvalho de Melo <cjesseantonio@yahoo.com>
2021-07-18 08:53:20 +0000

    initial commit

--- myWebsite/web_page.py
+++ myWebsite/web_page.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+# import secrets, then type secrets.token_hex(16) in a
+# python interpreter to get a secret key (hint: type python3 in the terminal)
+
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = '6e11de6809085499d0b65a8a00aeabc4'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
 
 @app.route("/")                          # this tells you the URL the method below is related to
 @app.route("/home")
@@ -10,7 +31,20 @@ def home():
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
   
+
 #def hello_world():
     #return "<p>Hello, World!</p>"        # this prints HTML to the webpage
   
